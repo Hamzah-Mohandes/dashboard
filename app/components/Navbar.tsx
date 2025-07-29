3eac98ebe09f8a761954c54025829cb917f3e389
Hamzah <rootmoodrm@gmail.com>
2025-07-29 18:53:30 +0200

    Dynamic  navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,18 @@ import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
+
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "Store", href: "/store" },
+    ]
+
+
     return (
-        <nav className='flex gap-4 bg-gray-700 p-4 text-white text-xl'>
-            <Link href="/" className=' hover:bg-gray-600 p-2 rounded-xl transition-all duration-300 hover:text-gray-100 hover:scale-120  '>Home</Link>
-            <Link href="/store" className='hover:bg-gray-600 p-2 rounded-xl transition-all duration-300 hover:text-gray-100 hover:scale-120  '>Store</Link>
+        <nav className='flex gap-4 bg-gray-700 p-4 text-white text-lg'>
+            {navLinks.map((link, index) => (
+                <Link key={index} href={link.href} className=' hover:bg-gray-600 p-2 rounded-xl transition-all  duration-300 hover:text-gray-100 hover:scale-120  '>{link.name}</Link>
+            ))}
         </nav>
     )
 }
