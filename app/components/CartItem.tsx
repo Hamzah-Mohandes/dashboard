df9dcfb7e1f66b9e11bb3138a07986e0418102b0
Hamzah <rootmoodrm@gmail.com>
2025-08-01 15:18:23 +0200

    cartItem style Updated

--- app/components/CartItem.tsx
+++ app/components/CartItem.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CartItem() {
     return (
         <div className='grid grid-cols-12 mt-6'>
-            <div className='col-span-9 bg-gray-100 p-4 rounded-lg shadow-xl '>
+            <div className='col-span-9 bg-gray-100 p-4 rounded-lg shadow-xl h-64 '>
                 <h2 className='text-2xl font-bold text-orange-600 mb-4'>LE Spring 2023</h2>
                 <p className='text-gray-600 mb-4 text-lg font-semi'>LE Spring 2023</p>
                 <p className='text-gray-800 font-semibold mb-4'>Product price <span className='text-orange-600 font-bold text-xl'> 100 $</span></p>
@@ -13,8 +13,8 @@ function CartItem() {
 
 
             </div>
-            <div className='col-span-3 bg-gray-100 p-4 rounded-lg shadow-xl '>
-                <img src="https://images.squarespace-cdn.com/content/v1/5935fc072e69cf2cca7ff658/1684437574257-Y1NJGH6IU443D3BKL0YO/LE_Spring_2023-30.jpg?format=1000w" alt="" />
+            <div className='col-span-3 bg-gray-100 p-4 rounded-lg shadow-xl h-64 '>
+                <img className='w-full h-full object-contain rounded-lg hover:scale-150 transition-all duration-300 hover:shadow-xl cursor-pointer' src="https://images.squarespace-cdn.com/content/v1/5935fc072e69cf2cca7ff658/1684437574257-Y1NJGH6IU443D3BKL0YO/LE_Spring_2023-30.jpg?format=1000w" alt="" />
             </div>
         </div>
     )
